9dc0d537950d3ba9453a6ebc6f1b49cc3add9b74
akutuzov <akutuzov72@gmail.com>
2018-04-25 00:10:45 +0200

    Chinese tokenization with Stanford Segmenter

--- 158_tokenizer/158_tokenizer.sh
+++ 158_tokenizer/158_tokenizer.sh
@@ -10,7 +10,13 @@
 while read x
 do
     DET_LANG=`echo ${x} | language_identification/fasttext predict language_identification/lid.176.ftz -`
-    echo ${x} | Europarl/tokenizer.perl -l ${DET_LANG##*__}
+    if [ "${DET_LANG##*__}" == "zh" ]; then
+	echo ${x} > temp.txt
+	stanford_segmenter/segment.sh pku temp.txt UTF-8 0
+	rm temp.txt
+    else
+	echo ${x} | Europarl/tokenizer.perl -l ${DET_LANG##*__}
+    fi
 done
 
 
