263954f7970f0ea1ceee53663219b0c264f47c49
Denis Teslenko <teslenkoden@gmail.com>
2018-12-18 00:50:50 +0300

    Add initial disambiguator

--- 158_disambiguator/disambiguator.py
+++ 158_disambiguator/disambiguator.py
@@ -2,19 +2,30 @@
 
 import configparser
 
-from jsonrpcserver import methods
+from jsonrpcserver import dispatch, method
 from werkzeug.serving import run_simple
 from werkzeug.wrappers import Request, Response
 
+from egvi import WSD
 
-@methods.add
-def disambiguate(language, tokens):
-    pass
+
+@method
+def disambiguate(language, text):
+    if language == 'en':
+        inventory = "models/cc.en.300.vec.gz.top100.inventory.tsv"
+    elif language == 'ru':
+        inventory = "models/cc.ru.300.vec.gz.top200.wsi-inventory.tsv"
+    else:
+        return 'Wrong lang'
+    wsd = WSD(inventory, language=language, verbose=True)
+    results = [(word, wsd.get_best_sense_id(text, word, 5)) for word in text]
+
+    return results
 
 
 @Request.application
 def app(request):
-    r = methods.dispatch(request.data.decode())
+    r = dispatch(request.data.decode())
     return Response(str(r), r.http_status, mimetype='application/json')
 
 
