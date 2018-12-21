879bd80051ae27e980eee7f68efdd1fca3454a0d
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2018-12-21 16:48:41 +0100

    Add 158_frontend/test.py

---
+++ 158_frontend/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from jsonrpcclient import request
+
+text = 'I wrote my program in Python.'
+
+tokenization = request('http://localhost:5001/', 'tokenize', text).data.result
+
+print((tokenization['language'], tokenization['tokens']))
+
+disambiguation = request('http://localhost:5002/', 'disambiguate', tokenization['tokens']).data.result
+
+for senses in disambiguation:
+    sense = max(senses, key=lambda sense: sense['confidence'])
+    print('{}\t{}\t{:.2f}\t{}'.format(sense['token'], sense['keyword'], sense['confidence'],
+                                      ', '.join(sense['cluster']) if sense['cluster'] else ''))
