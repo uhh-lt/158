a942e4b3a40a13a171daef413db721b17506360d
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2018-12-21 16:00:12 +0100

    Update 158_tokenizer/test.py

--- 158_tokenizer/test.py
+++ 158_tokenizer/test.py
@@ -3,5 +3,5 @@
 from jsonrpcclient import request
 
 text = 'I wrote my program in Python.'
-result = request('http://localhost:5001/', 'tokenize', text=text).data.result
+result = request('http://localhost:5001/', 'tokenize', text).data.result
 print(result)
