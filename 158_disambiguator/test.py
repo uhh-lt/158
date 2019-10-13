f47a2cb8f0d0ce04f1f6773a7833e0405a8bd121
Pushkinue <teslenkoden@gmail.com>
2019-10-13 17:48:32 +0500

    Change test

--- 158_disambiguator/test.py
+++ 158_disambiguator/test.py
@@ -3,13 +3,13 @@
 from jsonrpcclient import request
 
 language_en = 'en'
-tokens_en = ['I', 'wrote', 'my', 'program', 'in', 'Python', '.']
+tokens_en = ['Ruby', 'Perl']
 
 result_en = request('http://localhost:5002/', 'disambiguate', language_en, tokens_en).data.result
 print(result_en)
 
 language_ru = 'ru'
-tokens_ru = ['пытка', 'дезертир']
+tokens_ru = ['щенок', 'такса']
 
 result_ru = request('http://localhost:5002/', 'disambiguate', language_ru, tokens_ru).data.result
 print(result_ru)
