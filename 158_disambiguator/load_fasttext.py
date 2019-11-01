fde35b80e486305fb52b716280d91d62a5aed291
Pushkinue <teslenkoden@gmail.com>
2019-11-01 19:44:01 +0500

    Add script to load fasttext models

---
+++ 158_disambiguator/load_fasttext.py
@@ -0,0 +1,69 @@
+import os
+from os.path import exists
+import requests
+from clint.textui import progress
+
+
+def ensure_dir(f):
+    """ Make the directory. """
+    if not os.path.exists(f):
+        os.makedirs(f)
+
+
+def download_word_embeddings(language):
+    """ Ensures that the word vectors exist by downloading them if needed. """
+
+    dir_path = os.path.join("models", language)
+    ensure_dir(dir_path)
+
+    filename = "cc.{}.300.vec.gz".format(language)
+    wv_fpath = os.path.join(dir_path, filename)
+    wv_pkl_fpath = wv_fpath + ".pkl"
+
+    if exists(wv_fpath):
+        print('Exists')
+    else:
+        wv_uri = "https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.{}.300.vec.gz".format(language)
+        print("Downloading the fasttext model from {}".format(wv_uri))
+        r = requests.get(wv_uri, stream=True)
+        with open(wv_fpath, "wb") as f:
+            total_length = int(r.headers.get("content-length"))
+            for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length / 1024) + 1):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+    return wv_fpath, wv_pkl_fpath
+
+
+def main():
+    lang_list = ['af', 'als', 'am', 'an', 'ar', 'arz', 'as',
+                 'ast', 'az', 'azb', 'ba', 'bar', 'bcl', 'be',
+                 'bg', 'bh', 'bn', 'bo', 'bpy', 'br', 'bs',
+                 'ca', 'ce', 'ceb', 'ckb', 'co', 'cs', 'cv',
+                 'cy', 'da', 'de', 'diq', 'dv', 'el', 'eml',
+                 'en', 'eo', 'es', 'et', 'eu', 'fa', 'fi',
+                 'fr', 'frr', 'fy', 'ga', 'gd', 'gl', 'gom',
+                 'gu', 'gv', 'he', 'hi', 'hif', 'hr', 'hsb',
+                 'ht', 'hu', 'hy', 'ia', 'id', 'ilo', 'io',
+                 'is', 'it', 'ja', 'jv', 'ka', 'kk', 'km',
+                 'kn', 'ku', 'ky', 'la', 'lb', 'li', 'lmo',
+                 'lt', 'lv', 'mai', 'mg', 'mhr', 'min', 'mk',
+                 'ml', 'mn', 'mr', 'mrj', 'ms', 'mt', 'mwl',
+                 'my', 'myv', 'mzn', 'nah', 'nap', 'nds', 'ne',
+                 'new', 'nl', 'nn', 'no', 'nso', 'oc', 'or',
+                 'os', 'pa', 'pam', 'pfl', 'pl', 'pms', 'pnb',
+                 'ps', 'pt', 'qu', 'rm', 'ro', 'ru', 'sa',
+                 'sah', 'sc', 'scn', 'sco', 'sd', 'sh', 'si',
+                 'sk', 'sl', 'so', 'sq', 'sr', 'su', 'sv',
+                 'sw', 'ta', 'te', 'tg', 'th', 'tk', 'tl',
+                 'tr', 'tt', 'ug', 'uk', 'ur', 'uz', 'vec',
+                 'vi', 'vls', 'vo', 'wa', 'war', 'xmf', 'yi',
+                 'yo', 'zea', 'zh', 'ko']
+    lang_list = ['en', 'ru', 'cs']
+    for lang in lang_list:
+        print(lang)
+        download_word_embeddings(language=lang)
+
+
+if __name__ == '__main__':
+    main()
