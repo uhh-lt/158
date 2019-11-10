8c5fc20aad30b9db80cb79dffba4e00e6a309726
Pushkinue <teslenkoden@gmail.com>
2019-11-10 21:22:54 +0500

    Add sqlite server for inventories

--- 158_disambiguator/sqlite_server.py
+++ 158_disambiguator/sqlite_server.py
@@ -4,8 +4,6 @@ import numpy as np
 
 
 class SqliteServer(object):
-    """Create a connection to the SQLite database with word vectors."""
-
     def __init__(self, db, lang):
         """:param db: database name
            :return: Connection object or None"""
@@ -37,13 +35,32 @@ class SqliteServer(object):
         return rows
 
     def __get_vocab__(self):
-        query = "SELECT WORD FROM {}".format(self.lang)
+        query = "SELECT word FROM {}".format(self.lang)
         rows = self.sql_query(query)
-        vocab = [item for sublist in rows for item in sublist]
+        vocab = set([item for sublist in rows for item in sublist])
         return vocab
+
+
+class SqliteServerModel(SqliteServer):
+    """Create a connection to the SQLite database with word vectors."""
+
+    def __init__(self, db, lang):
+        super().__init__(db, lang)
 
     def get_word_vector(self, word):
         query = "SELECT * FROM {table} WHERE word = '{word}'".format(table=self.lang, word=word)
         rows = self.sql_query(query)
         word_vector = np.array(rows[0][1:])
         return word_vector
+
+
+class SqliteServerInventory(SqliteServer):
+    """Create a connection to the SQLite database with language inventories."""
+
+    def __init__(self, db, lang):
+        super().__init__(db, lang)
+
+    def get_word_senses(self, word):
+        query = "SELECT * FROM {table} WHERE word = '{word}'".format(table=self.lang, word=word)
+        rows = self.sql_query(query)
+        return rows
