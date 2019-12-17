069fae64846bd95da4fe41ef6c88c3102257e989
Pushkinue <teslenkoden@gmail.com>
2019-12-17 22:23:38 +0500

    Slightly improve code for test

--- 158_disambiguator/test.py
+++ 158_disambiguator/test.py
@@ -2,18 +2,20 @@
 
 from jsonrpcclient import request
 
+disambiguator_url = 'http://localhost:5002/'
+
 language_en = 'en'
 
 word_en = 'Ruby'
-senses_en = request('http://localhost:5002/', 'senses', language_en, word_en).data.result
+senses_en = request(disambiguator_url, 'senses', language_en, word_en).data.result
 print(senses_en)
 
 tokens_en = ['Ruby', 'Perl']
-result_en = request('http://localhost:5002/', 'disambiguate', language_en, tokens_en).data.result
+result_en = request(disambiguator_url, 'disambiguate', language_en, tokens_en).data.result
 print(result_en)
 
 language_ru = 'ru'
 tokens_ru = ['щенок', 'такса']
 
-result_ru = request('http://localhost:5002/', 'disambiguate', language_ru, tokens_ru).data.result
+result_ru = request(disambiguator_url, 'disambiguate', language_ru, tokens_ru).data.result
 print(result_ru)
